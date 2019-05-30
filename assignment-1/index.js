220ed38f5a2b44ed8f5761aa58eec8c5b8531212
Shikhar Arora <shikhararora98@gmail.com>
2019-05-30 12:38:44 +0000

    Initial commit

---
+++ assignment-1/index.js
@@ -0,0 +1,11 @@
+const express=require('express')
+const app=express()
+
+const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
+
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
+app.listen(3000)
